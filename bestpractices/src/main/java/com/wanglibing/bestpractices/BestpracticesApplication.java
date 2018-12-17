699acef534d5c553f4943c15ba92ab199e8da39f
iamwlb <iamwanglibing@qq.com>
2018-12-17 10:37:08 +0800

    update

---
+++ bestpractices/src/main/java/com/wanglibing/bestpractices/BestpracticesApplication.java
@@ -0,0 +1,14 @@
+package com.wanglibing.bestpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestpracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestpracticesApplication.class, args);
+	}
+
+}
+
